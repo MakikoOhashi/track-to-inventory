c862dfd2a89bab6692bd8e0de1072c51a0f2323c
MakikoOhashi <makiron19831014@gmail.com>
2025-06-14 21:14:10 +0900

    re

--- app/utils/verifyShopifyHmac.ts
+++ app/utils/verifyShopifyHmac.ts
@@ -1,9 +1,7 @@
 import crypto from "crypto";
 
 export function verifyShopifyHmac(query: Record<string, string | string[]>, secret: string): boolean {
-  // hmacを取り出す
   const { hmac, ...rest } = query;
-  // クエリをアルファベット順にソートし、hmac以外で連結
   const message = Object.keys(rest)
     .sort()
     .map(key => {
@@ -12,6 +10,5 @@ export function verifyShopifyHmac(query: Record<string, string | string[]>, secr
     })
     .join('&');
   const hash = crypto.createHmac('sha256', secret).update(message).digest('hex');
-  // 比較（大文字小文字を合わせる）
-  return hash === hmac;
+  return typeof hmac === "string" && hash === hmac.toLowerCase();
 }
